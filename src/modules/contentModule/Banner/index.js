d756652d5ac8257d61ee7d5fdb5464e7613cf003
ShuyuanChuang <shuyuanchuang2020@gmail.com>
2023-04-29 03:14:06 +0800

    [fix](活動區): RWD 調整

---
+++ src/modules/contentModule/Banner/index.js
@@ -0,0 +1,11 @@
+const Banner = (props) => {
+    const { title } = props || {};
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Banner;
--- src/modules/contentModule/HomePage/index.js
+++ src/modules/contentModule/HomePage/index.js
@@ -1,10 +1,12 @@
 import Card from '@/components/Card'
 import News from '@/modules/contentModule/News'
 import Activity from '../Activity';
+import Banner from '../Banner';
 
 const HomePage = () => {
     return (
         <div>
+            <Banner/>
             <Activity title='營隊專區' />
             <News title='新鮮乾貨' />
             <Activity title='精選活動' />
