d5f6df1ff9c0d3e1d7302f41d482696f379ecbe8
ShuyuanChuang <shuyuanchuang2020@gmail.com>
2023-05-02 00:49:09 +0800

    [fix](活動卡片): 文字調整

--- src/modules/contentModule/Activity/index.js
+++ src/modules/contentModule/Activity/index.js
@@ -5,14 +5,14 @@ import Button from '@/components/Button';
 import { BUTTON_TYPES } from '@/data/button';
 
 const Activity = (props) => {
-    const { title, bgColor } = props || {};
+    const { title, bgColor, moreText } = props || {};
 
     return (
         <div className={classes.container} style={{ backgroundColor: bgColor}}>
             <div className={classes.container__content}>
                 <h2 className={classes.container__content__title} >{title}</h2>
                 <Card type='activity' />
-                <Button type={BUTTON_TYPES.TEXT} text='更多乾貨' />
+                <Button type={BUTTON_TYPES.TEXT} text={moreText} />
             </div>
         </div>
     );
