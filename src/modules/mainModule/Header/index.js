79b457a2789ea950a2b39e5ce71e841a72dead12
ShuyuanChuang <shuyuanchuang2020@gmail.com>
2023-05-02 16:45:00 +0800

    [fix](NavBar): Header RWD

--- src/modules/mainModule/Header/index.js
+++ src/modules/mainModule/Header/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import { BiMenuAltRight } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
@@ -15,10 +15,37 @@ import SearchBar from '@/components/SearchBar';
 
 const Header = () => {
     const [sticky, setSticky] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [size, setSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (size.width > 768 && menuOpen) {
+            setMenuOpen(false);
+        }
+    }, [size.width, menuOpen]);
+
+    const menuToggleHandler = () => {
+        setMenuOpen((p) => !p);
+    };
 
     useEffect(() => {
         const handleScroll = () => {
-            setSticky(window.pageYOffset > 200);
+            setSticky(window.pageYOffset > 180);
             console.log(window.pageYOffset);
         }
         window.addEventListener('scroll', handleScroll);
@@ -26,7 +53,7 @@ const Header = () => {
     })
 
     return (
-        <header className={sticky ? `${classes.header} ${classes.sticky}`: `${classes.header}`}>
+        <header className={sticky ? `${classes.header} ${classes.sticky}` : `${classes.header}`}>
             <div className={classes.header__content}>
                 <Link href="/" className={classes.header__content__logo}>
                     <Image src='/img/eport.png' alt='Eport' width='144' height='45' />
@@ -35,7 +62,7 @@ const Header = () => {
                     <Dropdown />
                     <SearchBar />
                 </div>
-                <nav className={`${classes.header__content__nav}`}>
+                <nav className={`${classes.header__content__nav} ${menuOpen && size.width < 768 ? classes.isMenu : ""}`}>
                     <ul>
                         <li>
                             <Link href="/" legacyBehavior>
@@ -56,7 +83,11 @@ const Header = () => {
                     <Button type={BUTTON_TYPES.PRIMARY} text='登入' />
                 </nav>
                 <div className={classes.header__content__toggle}>
-                        <BiMenuAltRight />
+                    {menuOpen ? (
+                        <BiMenuAltRight onClick={menuToggleHandler} />
+                    ) : (
+                        <AiOutlineClose onClick={menuToggleHandler} />
+                    )}
                 </div>
             </div>
         </header>
